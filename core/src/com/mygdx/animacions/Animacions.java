7e03145bb972492fb6be8b34937c0cb6d527c01f
EduardMacianHerrera <95917835+EduardMacianHerrera@users.noreply.github.com>
2023-05-18 17:29:54 +0200

    Compila y renderiza correctamente

--- core/src/com/mygdx/animacions/Animacions.java
+++ core/src/com/mygdx/animacions/Animacions.java
@@ -1,5 +1,4 @@
 package com.mygdx.animacions;
-
 import com.badlogic.gdx.Application;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
@@ -10,205 +9,212 @@ import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
-import com.badlogic.gdx.utils.ScreenUtils;
 import com.github.czyzby.websocket.WebSocket;
 import com.github.czyzby.websocket.WebSocketListener;
 import com.github.czyzby.websocket.WebSockets;
 
 public class Animacions extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
 
-	// Constant rows and columns of the sprite sheet
-	private static final int FRAME_COLS = 4, FRAME_ROWS = 4;
-
-	// Objects used
-	Animation<TextureRegion> walkAnimation; // Must declare frame type (TextureRegion)
-	Texture walkSheet;
+	Texture walkSheet, background;
 	SpriteBatch spriteBatch;
+	TextureRegion idleFrames[] = new TextureRegion[4];
+	TextureRegion walkingFrames[] = new TextureRegion[4];
+
 	private OrthographicCamera camera;
 
-	// A variable for tracking elapsed time for the animation
+	Animation<TextureRegion> idleAnimation, walkingAnimation;
 	float stateTime;
-	float scaleFactor = 0.3f;
+	SpriteBatch batch;
+	float posx = 512, posy = 266;
 
-	//Player movement
-	private float playerPosX;
-	private float playerPosY;
-	private Rectangle reclangleUp, rectangleDown, rectangleLeft, rectangleRight;
-	private float playerSpeed = 220;
-	private Vector3 playerPos;
-	private Vector3 playerDirectionLeft, playerDirectionRight, playerDirectionUp, playerDirectionDown, playerIdle;
-	private int playerDirection;
+	Rectangle rectangleUp, rectangleDown, rectangleLeft, rectangleRight;
+	final int IDLE=0, UP=1, DOWN=2, LEFT=3, RIGHT=4;
+	float lastSend = 0f;
 
-	//Websocket
 	WebSocket socket;
 	String address = "localhost";
 	int port = 8888;
 
-	float lastSend = 0;
-
 	@Override
-	public void create () {
+	public void create() {
 
+		walkSheet = new Texture(Gdx.files.internal("mario.png"));
+		background = new Texture(Gdx.files.internal("backgroundx2.png"));
+
+		// Frames cuando esta quieto
+		idleFrames[0] = new TextureRegion(walkSheet,4,122,20,39);
+		idleFrames[1] = new TextureRegion(walkSheet,31,122,20,39);
+		idleFrames[2] = new TextureRegion(walkSheet,62,122,20,39);
+		idleFrames[3] = new TextureRegion(walkSheet,90,122,20,39);
+
+		// Frames andar (se giran cuando anda hacia la izquierda)
+		walkingFrames[0] = new TextureRegion(walkSheet,21,0,20,39);
+		walkingFrames[1] = new TextureRegion(walkSheet,42,0,20,39);
+		walkingFrames[2] = new TextureRegion(walkSheet,64,0,20,39);
+		walkingFrames[3] = new TextureRegion(walkSheet,85,0,20,39);
+
+		idleAnimation = new Animation<TextureRegion>(0.3f, idleFrames);
+		walkingAnimation = new Animation<TextureRegion>(0.3f, walkingFrames);
 
 		camera = new OrthographicCamera();
-		camera.setToOrtho(false, 800, 480);
-
-		// Load the sprite sheet as a Texture
-		walkSheet = new Texture(Gdx.files.internal("AmongSpritesheet1024x1024_02.png"));
-
-		// Use the split utility method to create a 2D array of TextureRegions. This is
-		// possible because this sprite sheet contains frames of equal size and they are
-		// all aligned.
-		TextureRegion[][] tmp = TextureRegion.split(walkSheet,
-				walkSheet.getWidth() / FRAME_COLS,
-				walkSheet.getHeight() / FRAME_ROWS);
-
-		// Place the regions into a 1D array in the correct order, starting from the top
-		// left, going across first. The Animation constructor requires a 1D array.
-		TextureRegion[] walkFrames = new TextureRegion[FRAME_COLS * FRAME_ROWS];
-		int index = 0;
-		for (int i = 0; i < FRAME_ROWS; i++) {
-			for (int j = 0; j < FRAME_COLS; j++) {
-				walkFrames[index++] = tmp[i][j];
-			}
-		}
-
-		// Initialize the Animation with the frame interval and array of frames
-		walkAnimation = new Animation<TextureRegion>(0.15f, walkFrames);
-
-		// Instantiate a SpriteBatch for drawing and reset the elapsed animation
-		// time to 0
-		spriteBatch = new SpriteBatch();
+		camera.setToOrtho(false, 1024, 512);
+		batch = new SpriteBatch();
 		stateTime = 0f;
 
+		// Controles
+		rectangleUp = new Rectangle(0, 512*2/3, 1024, 512/3);
+		rectangleDown = new Rectangle(0, 0, 1024, 512/3);
+		rectangleLeft = new Rectangle(0, 0, 1024/3,512);
+		rectangleRight = new Rectangle(1024*2/3, 0, 1024/3, 512);
 
-		playerPos = new Vector3((512 - (walkSheet.getWidth() / 4)) + 300,50,0);
-
-		reclangleUp = new Rectangle(0, 480 * 2/3, 800, 480 /3);
-		rectangleDown = new Rectangle(0, 0, 800, 480/3);
-		rectangleLeft = new Rectangle(0, 0, 800/3, 480);
-		rectangleRight = new Rectangle(800 *2/3, 0, 800/3, 480);
-
-		playerDirectionUp = new Vector3(0,1,0);
-		playerDirectionDown = new Vector3(0,-1,0);
-		playerDirectionRight = new Vector3(1,0,0);
-		playerDirectionLeft = new Vector3(-1,0,0);
-		playerIdle = new Vector3(0,0,0);
-		playerDirection = 1;
-
-
-		//Websocket
-		if( Gdx.app.getType()== Application.ApplicationType.Android ) {
+		// Abrir Websocket
+		if( Gdx.app.getType()== Application.ApplicationType.Android )
 			// en Android el host és accessible per 10.0.2.2
 			address = "10.0.2.2";
-			socket = WebSockets.newSocket(WebSockets.toWebSocketUrl(address, port));
-			socket.setSendGracefully(false);
-			socket.addListener((WebSocketListener) new MyWSListener());
-			socket.connect();
-			socket.send("Hola");
-		}
+		socket = WebSockets.newSocket(WebSockets.toWebSocketUrl(address, port));
+		socket.setSendGracefully(false);
+		socket.addListener((WebSocketListener) new MyWSListener());
+		socket.connect();
+		socket.send("Test");
 	}
-
 
 	@Override
-	public void render () {
+	public void resize(int width, int height) {
+	}
 
-		ScreenUtils.clear(0.447f, 0.533f, 0.569f, 1);
+	@Override
+	public void render() {
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		stateTime += Gdx.graphics.getDeltaTime();
+
 		camera.update();
 
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); // Clear screen
-		stateTime += Gdx.graphics.getDeltaTime(); // Accumulate elapsed animation time
+		TextureRegion frame = idleAnimation.getKeyFrame(stateTime,true);
+		TextureRegion walkCurrentFrame = walkingAnimation.getKeyFrame(stateTime,true);
 
-		// Get current frame of animation for the current stateTime
-		TextureRegion currentFrame = walkAnimation.getKeyFrame(stateTime, true);
+		batch.begin();
+		batch.setProjectionMatrix(camera.combined);
+		batch.draw(background, 0 , 0, 1024, 512);
+		batch.end();
 
-		if(playerDirection == -1){
-			currentFrame.flip(true, false);
-		}else{
-			currentFrame.flip(false, false);
+		int direction = virtual_joystick_control();
+
+		switch (direction){
+			case 0: // Quieto
+				batch.begin();
+				idleAnimation = new Animation<TextureRegion>(0.3f, idleFrames);
+				batch.draw(frame, posx, posy, 0, 0,
+						frame.getRegionWidth(),frame.getRegionHeight(),2,2,0);
+				batch.end();
+				break;
+			case 1: // Arriba
+				idleAnimation = new Animation<TextureRegion>(0.3f, walkingFrames);
+				batch.begin();
+				posy += 5;
+				batch.draw(walkCurrentFrame, posx, posy,0, 0,
+						walkCurrentFrame.getRegionWidth(),walkCurrentFrame.getRegionHeight(),2,2,0);
+				batch.end();
+				break;
+			case 2: // Abajo
+				idleAnimation = new Animation<TextureRegion>(0.3f, walkingFrames);
+				batch.begin();
+				posy -= 5;
+				batch.draw(walkCurrentFrame, posx, posy,0, 0,
+						walkCurrentFrame.getRegionWidth(),walkCurrentFrame.getRegionHeight(),2,2,0);
+				batch.end();
+				break;
+			case 3: // Izquierda
+				idleAnimation = new Animation<TextureRegion>(0.3f, walkingFrames);
+				batch.begin();
+				posx -= 5;
+				batch.draw(walkCurrentFrame, posx, posy,walkCurrentFrame.getRegionWidth(), 0,
+						walkCurrentFrame.getRegionWidth(),walkCurrentFrame.getRegionHeight(),-2,2,0); // Se pone el scaleX en negativo para girarlo del reves
+				batch.end();
+				break;
+			case 4: // Derecha
+				idleAnimation = new Animation<TextureRegion>(0.3f, walkingFrames);
+				batch.begin();
+				posx += 5;
+				batch.draw(walkCurrentFrame, posx, posy, 0, 0,
+						walkCurrentFrame.getRegionWidth(), walkCurrentFrame.getRegionHeight(), 2, 2, 0);
+				batch.end();
+				break;
 		}
 
-		spriteBatch.setProjectionMatrix(camera.combined);
-		spriteBatch.begin();
-		spriteBatch.draw(currentFrame,  playerPos.x += ((getPlayerDirection().x * playerSpeed) * Gdx.graphics.getDeltaTime()) , playerPos.y += ((getPlayerDirection().y * playerSpeed) * Gdx.graphics.getDeltaTime()), scaleFactor * currentFrame.getRegionWidth(), scaleFactor * currentFrame.getRegionHeight());
-		spriteBatch.end();
-
-		currentFrame.flip(currentFrame.isFlipX(), currentFrame.isFlipY());
-
+		// Envio websockets
 		if( stateTime-lastSend > 1.0f ) {
 			lastSend = stateTime;
-			socket.send("Player position: X=" + playerPos.x  + ", Y=" + playerPos.y);
+			socket.send("Posicion x:"+posx+", y:"+posy);
 		}
+	}
+
+	@Override
+	public void pause() {
 
 	}
 
+	@Override
+	public void resume() {
 
-	private Vector3 getPlayerDirection() {
-		// iterar per multitouch
-		// cada "i" és un possible "touch" d'un dit a la pantalla
+	}
+
+	@Override
+	public void dispose() {
+		spriteBatch.dispose();
+		walkSheet.dispose();
+		background.dispose();
+	}
+
+	protected int virtual_joystick_control() {
 		for(int i=0;i<10;i++)
 			if (Gdx.input.isTouched(i)) {
 				Vector3 touchPos = new Vector3();
 				touchPos.set(Gdx.input.getX(i), Gdx.input.getY(i), 0);
-				// traducció de coordenades reals (depen del dispositiu) a 800x480
 				camera.unproject(touchPos);
-				if (reclangleUp.contains(touchPos.x, touchPos.y)) {
-					return playerDirectionUp;
+				if (rectangleUp.contains(touchPos.x, touchPos.y)) {
+					return UP;
 				} else if (rectangleDown.contains(touchPos.x, touchPos.y)) {
-					return playerDirectionDown;
+					return DOWN;
 				} else if (rectangleLeft.contains(touchPos.x, touchPos.y)) {
-					playerDirection = -1;
-					return playerDirectionLeft;
+					return LEFT;
 				} else if (rectangleRight.contains(touchPos.x, touchPos.y)) {
-					playerDirection = 1;
-					return playerDirectionRight;
+					return RIGHT;
 				}
 			}
-		return playerIdle;
+		return IDLE;
 	}
 
+	class MyWSListener implements WebSocketListener {
 
-	@Override
-	public void dispose () { // SpriteBatches and Textures must always be disposed
-		spriteBatch.dispose();
-		walkSheet.dispose();
-	}
-}
+		@Override
+		public boolean onOpen(WebSocket webSocket) {
+			System.out.println("Opening...");
+			return false;
+		}
 
+		@Override
+		public boolean onClose(WebSocket webSocket, int closeCode, String reason) {
+			System.out.println("Closing...");
+			return false;
+		}
 
-class MyWSListener implements WebSocketListener {
+		@Override
+		public boolean onMessage(WebSocket webSocket, String packet) {
+			System.out.println("Message:");
+			return false;
+		}
 
-	@Override
-	public boolean onOpen(WebSocket webSocket) {
-		System.out.println("Opening...");
-		return false;
-	}
+		@Override
+		public boolean onMessage(WebSocket webSocket, byte[] packet) {
+			System.out.println("Message:");
+			return false;
+		}
 
-	@Override
-	public boolean onClose(WebSocket webSocket, int closeCode, String reason) {
-		System.out.println("Closing...");
-		return false;
-	}
-
-	@Override
-	public boolean onMessage(WebSocket webSocket, String packet) {
-		System.out.println("Message:");
-		return false;
-	}
-
-	@Override
-	public boolean onMessage(WebSocket webSocket, byte[] packet) {
-		System.out.println("Message:");
-		return false;
-	}
-
-	@Override
-	public boolean onError(WebSocket webSocket, Throwable error) {
-		System.out.println("ERROR:"+error.toString());
-		return false;
+		@Override
+		public boolean onError(WebSocket webSocket, Throwable error) {
+			System.out.println("ERROR:"+error.toString());
+			return false;
+		}
 	}
 }
